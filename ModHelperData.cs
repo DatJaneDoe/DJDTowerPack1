6c2314b7c7b2433605dce7fd3ae0d5d4a26fea55
DatJaneDoe <54993349+DatJaneDoe@users.noreply.github.com>
2023-12-05 10:22:31 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace DJDTowerPack;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "39.0";
-    public const string Version = "1.0.2";
+    public const string Version = "1.1.0";
     public const string Name = "DatJaneDoe's Tower Pack 1";
 
     public const string Description = "Adds Necromancer, Pizza, Soldier, and Teacher towers to the game";
