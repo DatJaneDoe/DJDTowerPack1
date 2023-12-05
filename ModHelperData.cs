7e426bf6b1dfdbc7a896288e328765c4afc3134b
DatJaneDoe <54993349+DatJaneDoe@users.noreply.github.com>
2023-12-05 10:51:13 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -7,8 +7,6 @@ public static class ModHelperData
     public const string Name = "DatJaneDoe's Tower Pack 1";
 
     public const string Description = "Adds Necromancer, Pizza, Soldier, and Teacher towers to the game";
-
-    public const string Icon = "pack1.png";
     public const string RepoOwner = "DatJaneDoe";
     public const string RepoName = "DJDTowerPack1";
 
